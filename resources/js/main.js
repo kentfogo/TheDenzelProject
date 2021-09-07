242b08454dc895422d254d439c2914115bc8c644
kwroburr <robertsonwkent@gmail.com>
2021-09-07 18:23:01 -0400

    changed header element

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,12 @@
 
 
+
 var slides = document.querySelectorAll('.slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 5000);
+var slideInterval = setInterval(nextSlide, 3000);
 
 function nextSlide() {
   slides[currentSlide].className = 'slide';
   currentSlide = (currentSlide+1)%slides.length;
   slides[currentSlide].className = 'slide showing';
 }
-
-// textFit(document.querySelector("h1"));
