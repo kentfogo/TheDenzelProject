72c804bbcc646387c6c1ba5fccf7e06781de1a3f
kwroburr <robertsonwkent@gmail.com>
2021-09-12 16:40:20 -0400

    added horizontal scrolling effects

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,4 @@
-
-
-
+//slideshow effect
 var slides = document.querySelectorAll('.slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 3000);
@@ -9,4 +7,25 @@ function nextSlide() {
   slides[currentSlide].className = 'slide';
   currentSlide = (currentSlide+1)%slides.length;
   slides[currentSlide].className = 'slide showing';
+}
+
+
+//horizontal scrolling effect
+var windowWidth = window.innerWidth;
+var horizontalLength = document.querySelector(".card-wrapper").scrollWidth;
+var distFromTop = document.querySelector(".bio-container").offsetTop;
+var scrollDistance = distFromTop + horizontalLength - windowWidth;
+
+console.log(scrollDistance);
+
+document.querySelector(".bio-container").style.height = horizontalLength + "px";
+
+window.onscroll = function() {
+  var scrollTop - window.pageYOffset;
+
+if(scrollTop >= distFromTop && scrollTop <= scrollDistance){
+  document.querySelector(".card-wrapper").style.transform = "translateX(-" +(scrollTop - distFromTop) + "px)";
+}
+
+  console.log(scrollTop)
 }
